b4d19e2de0be9af89527329ce8990995af1be544
Dat T Vu <dtvutrong1993@gmail.com>
2023-03-20 00:36:49 +0700

    add: methods to handle invoke input add: debug by print invoke input to screen

--- Source/SpaceVikings/PlayerCharacter.cpp
+++ Source/SpaceVikings/PlayerCharacter.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "PlayerCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h" 
-#include "PlayerCharacter.h"
+
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
@@ -19,6 +20,8 @@ APlayerCharacter::APlayerCharacter()
 	m_FollowTopDownCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	//Set FollowCamera attachment to the Camera Boom
 	m_FollowTopDownCamera->SetupAttachment(m_CameraBoom);
+	//Init vector to hold value for orb combination
+	m_vecOrbCombination.Init(EOrb::NONE, m_maxOrbCnt);
 }
 
 // Called when the game starts or when spawned
@@ -44,13 +47,76 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	// Set up movements
 	PlayerInputComponent->BindAxis("MoveForward", this, &ACharacterBase::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ACharacterBase::MoveRight);
-	// Set up "look" bindings.
-	//PlayerInputComponent->BindAxis("Turn", this, &ASpaceVikingsCharacterBase::AddControllerYawInput);
-	//PlayerInputComponent->BindAxis("LookUp", this, &ASpaceVikingsCharacterBase::AddControllerPitchInput);
 	// Set up "jump" bindings.
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacterBase::StartJump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacterBase::StopJump);
 	// Set up "fire" bindings
 	PlayerInputComponent->BindAction("Fire", IE_Released, this, &ACharacterBase::Fire);
+	// Set up invoke orb bindings
+	PlayerInputComponent->BindAction("Quas", IE_Released, this, &APlayerCharacter::HandleQuasPressed);
+	PlayerInputComponent->BindAction("Wex", IE_Released, this, &APlayerCharacter::HandleWexPressed);
+	PlayerInputComponent->BindAction("Exort", IE_Released, this, &APlayerCharacter::HandleExortPressed);
+	PlayerInputComponent->BindAction("Invoke", IE_Released, this, &APlayerCharacter::HandleInvorkPressed);
 }
 
+
+void APlayerCharacter::HandleQuasPressed() {
+	HanddleOrb(EOrb::QUAS);
+}
+
+void APlayerCharacter::HandleWexPressed() {
+	HanddleOrb(EOrb::WEX);
+}
+
+void APlayerCharacter::HandleExortPressed() {
+	HanddleOrb(EOrb::EXORT);
+}
+
+void APlayerCharacter::HanddleOrb(const EOrb eVal) {
+	m_vecOrbCombination[m_currentOrbSlot] = eVal;
+	m_currentOrbSlot = (m_currentOrbSlot + 1) % m_maxOrbCnt;
+}
+
+void APlayerCharacter::HandleInvorkPressed() {
+	
+	int invokeSum = 0;
+
+	for (auto num : m_vecOrbCombination) {
+		invokeSum += num;
+	}
+#ifdef UE_BUILD_DEBUG
+	FString skillInvoked = "";
+	switch (invokeSum) {
+		case EInvoke::QQ:
+			skillInvoked = "QQ";
+			break;
+		case EInvoke::QW:
+			skillInvoked = "QW / WQ";
+			break;
+		case EInvoke::QE:
+			skillInvoked = "QE / EQ";
+			break;
+		case EInvoke::WW:
+			skillInvoked = "WW";
+			break;
+		case EInvoke::WE:
+			skillInvoked = "WE / EW";
+			break;
+		case EInvoke::EE:
+			skillInvoked = "EE";
+			break;
+		case EInvoke::NN:
+		case EInvoke::QN:
+		case EInvoke::WN:
+		case EInvoke::EN:
+		default:
+			skillInvoked = "Invalid";
+			break;
+	}
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	check(GEngine != nullptr);
+	GEngine->AddOnScreenDebugMessage(0, 1.0f, FColor::Red, skillInvoked);
+#else
+#endif
+}
--- Source/SpaceVikings/PlayerCharacter.h
+++ Source/SpaceVikings/PlayerCharacter.h
@@ -10,15 +10,37 @@ UCLASS()
 class SPACEVIKINGS_API APlayerCharacter : public ACharacterBase
 {
 	GENERATED_BODY()
-
 public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
 
+private:
+	//TO-DO: refactore this enum. now it needs to declare infront of TArray
+	enum EOrb {
+		NONE = 0,
+		QUAS = 1,
+		WEX = 7,
+		EXORT = 11
+	};
+	enum EInvoke {
+		NN = 0,
+		QN = 1,
+		WN = 7,
+		EN = 11,
+		QQ = 2,
+		QW = 8,
+		QE = 12,
+		WW = 14,
+		WE = 18,
+		EE = 22
+	};
+	TArray<EOrb> m_vecOrbCombination;
+	int m_currentOrbSlot = 0;
+	const int m_maxOrbCnt = 2;
+	FORCEINLINE void HanddleOrb(const EOrb eVal);
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 	// TopDown camera.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Camera)
 	class USpringArmComponent* m_CameraBoom;
@@ -28,8 +50,15 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION()
+	void HandleQuasPressed();
+	UFUNCTION()
+	void HandleWexPressed();
+	UFUNCTION()
+	void HandleExortPressed();
+	UFUNCTION()
+	void HandleInvorkPressed();
 };
